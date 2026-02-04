f877fad218acef47169e195bf6793c9982e6a5f7
sud0pacman <mukhammad.kammoliddin@gmail.com>
2026-02-04 22:36:42 +0500

    darwin user fix syntax for mkFlake

--- modules/darwin/users/default.nix
+++ modules/darwin/users/default.nix
@@ -1,5 +1,43 @@
-# Keep all specific user configs here as a module
 {
-  # List your users here
-  muhammad = import ./muhammad.nix;
+  pkgs,
+  inputs,
+  ...
+}: let 
+  username = "muhammad";
+in {
+  config = {
+    system.primaryUser = username;
+
+    home-manager.backupFileExtension = "hbak";
+
+    # Available users in the machine
+    users.users = {
+      ${username} = {
+        home = "/Users/${username}";
+        description = "Muhammad";
+        shell = pkgs.zsh;
+
+        # openssh.authorizedKeys.keys = lib.strings.splitString "\n" (
+        #   builtins.readFile (
+        #     builtins.fetchurl {
+        #       url = "https://github.com/lambdajon.keys";
+        #       sha256 = "440dac32fb3ecd060c17f78ad7c34422fefaaccf525c75c3c8dfd5ce86ef516e";
+        #     }
+        #   )
+        # );
+      };
+    };
+
+    # # Home manager configuration for users
+    # home-manager = {
+    #   extraSpecialArgs = {
+    #     inherit inputs outputs;
+    #   };
+    #   backupFileExtension = "baka";
+    #   users = {
+    #     # Import your home-manager configuration
+    #     muhammad = import ../../../home.nix;
+    #   };
+    # };
+  };
 }
