45f37e2a0132325853cc9c1e89dd695c86e0f62f
sud0pacman <mukhammad.kammoliddin@gmail.com>
2026-02-07 16:38:25 +0500

    boot config fix

--- modules/nixos/boot/default.nix
+++ modules/nixos/boot/default.nix
@@ -1,6 +1,39 @@
-# Keep all declarative bootloader configurations here
 {
-  # List all bootloaders here
-  grub = import ./grub.nix;
-  systemd = import ./systemd.nix;
+  pkgs,
+  inputs,
+  ...
+}: {
+  config = {
+    # Bootloader.
+    boot = {
+      loader = {
+        # After going back to bootloader, run this: 
+        # nixos-rebuild switch --install-bootloader
+        systemd-boot.enable = false;
+        efi.canTouchEfiVariables = true;
+        grub = {
+          enable = true;
+          devices = ["nodev"];
+          useOSProber = true;
+          efiSupport = true;
+          theme = "${
+            (pkgs.fetchFromGitHub {
+              owner = "xinux-org";
+              repo = "bootloader-theme";
+              tag = "v1.0.3";
+              hash = "sha256-ipaiJiQ3r2B3si1pFKdp/qykcpaGV+EqXRwl6UkCohs=";
+            })
+          }/xinux";
+        };
+      };
+      plymouth = {
+        enable = true;
+        theme = "mac-style";
+        themePackages = [inputs.mac-style-plymouth.packages."${pkgs.stdenv.hostPlatform.system}".default];
+      };
+      consoleLogLevel = 3;
+      initrd.systemd.enable = true;
+      initrd.verbose = false;
+    };
+  };
 }
